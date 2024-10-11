71f08c99f0a4af863a9f42eca4aad1c6a01db4fc
Natan Campos <nasocabr@gmail.com>
2024-10-11 16:35:27 -0300

    finalizando e colocando restrições para evitar possíveis erros

--- praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -3,6 +3,16 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de >= ate){
+        alert ('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
+    if (quantidade > (ate - de + 1 )){
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
+    }
+
     let sorteados = [];
 
     for (let i = 0; i < quantidade; i++) {
@@ -11,7 +21,7 @@ function sortear(){
         while (sorteados.includes(numero)){
             numero = gerarNumeroAleatorio(de,ate);
         }
-        
+
         sorteados.push(numero);
     }
 
@@ -20,5 +30,24 @@ function sortear(){
     }
     let resultado = document.getElementById('resultado');
         resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+        alterarStatusBotao();
 }
 
+function alterarStatusBotao(){
+    let botaoReiniciar = document.getElementById('btn-reiniciar');
+    if (botaoReiniciar.classList.contains('container__botao-desabilitado')){
+        botaoReiniciar.classList.remove('container__botao-desabilitado');
+        botaoReiniciar.classList.add('container__botao');
+    }else {
+        botaoReiniciar.classList.remove('container__botao');
+        botaoReiniciar.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value ='';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
