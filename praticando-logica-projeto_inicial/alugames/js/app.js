2357ec938dd4e418ee93435978fba89881e3afcd
Natan Campos <nasocabr@gmail.com>
2024-10-14 12:53:41 -0300

    adicionando confirmação de devolução

--- praticando-logica-projeto_inicial/alugames/js/app.js
+++ praticando-logica-projeto_inicial/alugames/js/app.js
@@ -1,15 +1,18 @@
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let imagem = gameClicado.querySelector('.dashboard__item__img img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    
+    if (botao.textContent === "Devolver") {
+        if (confirm(`Tem certeza de que deseja devolver ${nomeJogo.innerHTML}?`)) {
+            botao.textContent = "Alugar";
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+        }
     } else {
+        botao.textContent = "Devolver";
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
     }
 }
