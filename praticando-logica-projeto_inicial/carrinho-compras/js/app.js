830ec50cde0b9c6856a7e9ced8f4033a68db590f
Natan Campos <nasocabr@gmail.com>
2024-10-15 11:34:06 -0300

    tornando tudo funcional e adicionando proteção para possíveis erros

--- praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,11 +1,27 @@
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
+
 function adicionar(){
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
-
-    alert(nomeProduto.value);
-    alert(quantidade.value);
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    if (quantidade <= 0){
+        return
+    }
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
-
 function limpar (){
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
