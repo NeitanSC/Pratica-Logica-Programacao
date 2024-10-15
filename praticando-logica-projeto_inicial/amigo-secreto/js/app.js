31ac18f82024f8e4cfbe3b4ade665b399c34355b
Natan Campos <nasocabr@gmail.com>
2024-10-15 14:25:21 -0300

    deixando tudo funcional, botão de reiniciar, embaralho e sortear, além de algumas verificações para evitar erros

--- praticando-logica-projeto_inicial/amigo-secreto/js/app.js
+++ praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -1,8 +1,9 @@
+let amigos = [];
 function adicionar() {
     let nomeAmigo = document.getElementById('nome-amigo').value.trim();
     let listaAmigos = document.getElementById('lista-amigos');
-
-    if (nomeAmigo === '') {
+    amigos.push(nomeAmigo);
+    if (nomeAmigo === '' || nomeAmigo == nomeAmigo) {
         alert("Por favor, insira um nome válido.");
         return;
     }
@@ -14,8 +15,28 @@ function adicionar() {
     document.getElementById('nome-amigo').value = '';
 }
 function sortear() {
+    embaralhar(amigos);
+    let listaSorteio = document.getElementById('lista-sorteio');
 
+    for(let i = 0; i < amigos.length; i++){
+
+        if(i === amigos.length -1){
+            listaSorteio.innerHTML += amigos[i] + '-->' + amigos [0] + '<br>';
+        }else{
+        listaSorteio.innerHTML += amigos[i] + '-->' + amigos [i + 1] + '<br>';
+    }
+}
+}
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
 function reiniciar (){
-
+    amigos = [];
+    document.getElementById('nome-amigo').value.trim().innerHTML = '';
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    return;
 }
